4f4a8b7f94b4c2e8620517c981174c0a02d09097
TimmySigs <108436098+TimmySigs@users.noreply.github.com>
2022-10-12 23:40:23 -0500

    "refactored seeds.sql"

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,15 +1,31 @@
+INSERT INTO departments (name)
+VALUES 
+("Administration"),
+("Music"),
+("Visual");
 
-INSERT INTO employees (first_name, last_name, role_id, manager_id)
-VALUES ("John", "Lennon", 1, NULL), ("Paul", "McCartney", 2, 1), ("George", "Harrison", 3, NULL), ("Ringo", "Starr", 4, 3), ("Chuck", "Berry", 5, 5), ("Elvis", "Presly", 6, 5);
+INSERT INTO roles (title, salary, department_id)
+VALUES 
+("CEO", 125000, 1),
+("Tour Director", 70000, 1),
+("Administrative Intern", 100, 1),
+("Brass Arranger", 65000, 2),
+("Brass Caption Head", 45000, 2),
+("Brass Tech", 4000, 2),
+("Percussion Arranger", 65000, 2),
+("Percussion Caption Head", 45000, 2),
+("Percussion Tech", 4000, 2);
 
-INSERT INTO employees (first_name, last_name, role_id, manager_id)
-VALUES ("Bobby", "Jenks", 2, 1), ("Paul", "Konerko", 4, 3), ("Mark", "Burhrle", 6, 5);
 
+INSERT INTO employees (first_name, last_name, role_id, manager_id, is_manager)
+VALUES 
+("Paul", "Konerko", 1, null, 1),
+("Bobby", "Jenks", 2, 1, 1),
+("Joe", "Crede", 3, 2, 0),
+("Scotty", "Pods", 4, 1, 1),
+("Aaron", "Rowand", 5, 4, 1),
+("Frank", "Thomas", 6, 5, 0),
+("Freddy", "Garcia", 7, 1, 1),
+("Jose", "Contreras", 8, 7, 1),
+("Mark", "Buehrle", 9, 8, 0);
 
-
-INSERT INTO role (title, salary, department_id)
-VALUES ("Sales Lead", 80000, 1), ("Salesperson", 65000, 1), ("Legal Team Lead", 120000, 2), ("Lawyer", 105000, 2), ("Lead Engineer", 111000, 3), ("Engineer", 95000, 3);
-
-
-INSERT INTO department (name)
-VALUES ("Sales"), ("Legal"), ("Engineering");
