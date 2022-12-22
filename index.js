cf417bc7b2ffecbe8fc0713b208c02c36acb28b2
TimmySigs <108436098+TimmySigs@users.noreply.github.com>
2022-12-22 02:17:05 -0600

    reafctor db connection

--- index.js
+++ index.js
@@ -1,90 +1,32 @@
-// const cTable = require("console.table")
-// const inquirer = require("inquirer")
-// const { handlers } = require("./lib")
-// const mysql = require("mysql2")
-
-
-// const initializeApp = () => {
-//     inquirer.promt = ([
-//         {
-//             type: "list",
-//             name: "opening",
-//             message: "What would you like to do?",
-//             choices: [
-//         "Exit",
-//         "View all departments",
-//         "View all roles",
-//         "View all employees",
-//         "Add a department",
-//         "Add a role",
-//         "Add an employee",
-//         "Update a role's salary",
-//         "Update an employee's information",
-//         "Delete a department",
-//         "Delete a role",
-//         "Delete an employee",
-//     ]
-// }
-//     ])
-//     .then (answers.opening) 
-//         const openingChoices[0]:
-//             console.log("Goodbye!")
-//             return handlers.end()
-
-//         if openingChoices[1]:
-//             await handlers.view.departments()
-//             break
-
-//         case openingChoices[2]:
-//             await handlers.view.roles()
-//             break
-
-//         case openingChoices[3]:
-//             await handlers.view.employees()
-//             break
-
-//         case openingChoices[4]:
-//             await handlers.add.department()
-//             break
-
-//         case openingChoices[5]:
-//             await handlers.add.role()
-//             break
-
-//         case openingChoices[6]:
-//             await handlers.add.employee()
-//             break
-
-//         case openingChoices[7]:
-//             await handlers.update.role()
-//             break
-
-//         case openingChoices[8]:
-//             await handlers.update.employee()
-//             break
-
-//         case openingChoices[9]:
-//             await handlers.delete.department()
-//             break
-
-//         case openingChoices[10]:
-//             await handlers.delete.role()
-//             break
-
-//         case openingChoices[11]:
-//             await handlers.delete.employee()
-//             break
-//     }
-
-//     return initializeApp()
-
-
-// initializeApp()
-
 const cTable = require("console.table")
 const inquirer = require("inquirer")
 const { handlers } = require("./lib")
 const mysql = require("mysql2")
+
+require("dotenv").config()
+
+const db = mysql.createConnection(
+    {
+        host: "localhost",
+        user: "root",
+        password: "DB_PASSWORD",
+        database: "employees_db",
+    },
+    console.log("connected to the employee database")
+)
+
+
+db.connect(err => {
+    if (err) throw (err);
+    console.log("Connected to " + process.env.DB_NAME);
+    nowConnected();
+});
+
+nowConnected = () => {
+    console.log("EMPLOYEE MANAGER");
+    initializeApp();
+}
+
 
 const initializeApp = () => {
     inquirer.prompt([
